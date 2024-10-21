c05622697ed86f857a946eede0407b57017fbfed
karyne307 <heloyza.amorimsilva@gmail.com>
2024-10-21 12:06:53 -0300

    Add files via upload

---
+++ EX2110.c4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+    int estado;
+    char s;
+    char c;
+    char d;
+    char v;
+    printf ("qual e o estado civil?");
+  scanf("%d", &estado);
+  if(estado=='s'){
+    printf("solteiro");
+  }
+  if(estado=='c'){
+    printf("casado");
+  }
+  if(estado=='d'){
+  
+    printf("divorciado");
+  }
+  if(estado=='v'){
+    printf("viuvo");
+  }
+
+}
+
+
+
+
+
